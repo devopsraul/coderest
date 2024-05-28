2a9e8e43c4b60a45a3ca3df67987843aaeadab3e
devopsraul <devops.raul@gmail.com>
2024-05-28 09:04:34 -0300

    personaliza el metodo get

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from apps.users.models import User
-from apps.users.api.serializers import TestUserSerealizer, UserSerializer
+from apps.users.api.serializers import UserSerializer
 
 #creacion de la funcion api_view
 @api_view(['GET', 'POST'])
@@ -12,9 +12,9 @@ def user_api_view(request):
     #lista
     if request.method == 'GET':
         #queryset
-        users = User.objects.all()
-        users_serializer = UserSerializer(users, many=True)
-        
+        users = User.objects.all().values('id', 'username', 'email', 'password')
+        #print (users)
+        users_serializer = UserSerializer(users, many = True)        
         return Response(users_serializer.data, status = status.HTTP_200_OK)
 
     elif request.method == 'POST':
@@ -38,7 +38,7 @@ def user_detail_api_view(request, pk = None):
             return Response(user_serializer.data, status = status.HTTP_200_OK)
         #update
         elif request.method == 'PUT':
-            user_serializer = TestUserSerealizer(user, data = request.data, context = request.data)        
+            user_serializer = UserSerializer(user, data = request.data, context = request.data)        
             if user_serializer.is_valid():
                 user_serializer.save()
                 return Response(user_serializer.data, status = status.HTTP_200_OK) 
--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -4,38 +4,14 @@ from apps.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'#puede mencionar campo especidifcos con ['name', 'lastname',..]
+        fields = '__all__'#puede mencionar campo especificos con ('name', 'lastname',..)
         
-class TestUserSerealizer(serializers.Serializer):       
-    name = serializers.CharField(max_length = 255)
-    email = serializers.EmailField()
-
-    def validate_name(self, value):
-        if 'developer' in value:
-            raise serializers.ValidationError('Error, no puede existir un usuario con ese nombre')
-        print(value)
-        return value
-    
-    def validate_email(self, value):
-        #custom validation
-        if value == '':
-            raise serializers.ValidationError('Tiene que indicar un correo')
-        
-        if self.validate_name(self.context['name']) in value:
-            raise serializers.ValidationError('el email no puede contener el nombre')
-        
-        return value
-    
-    def validate(self, data):
-        return data 
-    
-    def create(self, validated_data):
-        return User.objects.create (**validated_data)
-    
-    def update(self, instance, validate_data):
-        #codigo interno que hace el serializers, por cada valors del modelo
-        instance.name = validate_data.get('name', instance.name)
-        instance.email = validate_data.get('email', instance.email)
-        instance.save()
-        print(instance)
-        return instance
+    def to_representation(self, instance):
+        #data = super().to_representation(instance)
+        #print(data)
+        return {
+            'id': instance['id'],
+            'nombre_usuario': instance['username'],
+            'correo': instance['email'],
+            'clave': instance['password'],
+        }
